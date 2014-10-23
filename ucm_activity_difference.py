a36cb0625eac42b125acc113d52d741751c9ab05
csauge <csauge@gmail.com>
2014-10-23 20:39:32 +0200

    Update and rename UCMActivity to ucm_activity_difference.py

---
+++ ucm_activity_difference.py
@@ -0,0 +1,29 @@
+import argparse # For ArgumentParser
+import subprocess # For check_output
+
+
+class UcmElement:
+    """Store UCM versions of an element"""
+    
+    def __init__(self):
+        version_list = []
+
+    
+
+class UcmActivityDifference:
+    """Manage a UCM activity change set"""
+
+    def __init__(self):
+        self.argparse()
+        self.get_change_set()
+
+    def argparse(self):
+        """Handle arguments of this module"""
+
+    def get_changet_set(self):
+        """Get change set of UCM activities"""
+
+        for activity_name in self.args.activity:
+            subprocess
+
+UcmActivityDifference()
